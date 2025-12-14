05719cc198a7f8de47b2c79cb40c83aeaa505f3c
aayushi200677 <Aayushi.24scse1010298@galgotiasuniversity.ac.in>
2025-12-14 12:56:06 +0530

    Update DoctorDao.java

--- DoctorDao.java
+++ DoctorDao.java
@@ -5,6 +5,11 @@ import java.util.List;
 public interface DoctorDao {
     int saveDoctor(Doctor doctor);
     List<Doctor> getAllDoctors();
+
+    boolean existsByName(String name);
+    List<Doctor> searchBySpecialization(String specialization);
+
     int updateDoctorExperience(String name, int experience);
     int deleteDoctor(String name);
 }
+
