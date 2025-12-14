870427494cf22b338ca19da68e4db37eb78ceb37
aayushi200677 <Aayushi.24scse1010298@galgotiasuniversity.ac.in>
2025-12-14 13:00:45 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,15 +1,15 @@
 package hospital;
 
 public class User {
+
     private int id;
     private String name;
     private int age;
     private String city;
 
-    // Default Constructor
-    public User() { }
+    public User() {
+    }
 
-    // All-Args Constructor
     public User(int id, String name, int age, String city) {
         this.id = id;
         this.name = name;
@@ -17,26 +17,54 @@ public class User {
         this.city = city;
     }
 
-    // Getters and Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    // Basic validation to improve robustness
+    public boolean isValid() {
+        return name != null && !name.trim().isEmpty()
+                && city != null && !city.trim().isEmpty()
+                && age > 0 && age <= 120;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public int getId() {
+        return id;
+    }
 
-    public int getAge() { return age; }
-    public void setAge(int age) { this.age = age; }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-    public String getCity() { return city; }
-    public void setCity(String city) { this.city = city; }
+    public String getName() {
+        return name;
+    }
 
-    // toString() for easy printing
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     @Override
     public String toString() {
-        return "User [id=" + id +
-               ", name=" + name +
-               ", age=" + age +
-               ", city=" + city + "]";
+        return "User{id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", city='" + city + '\'' +
+                '}';
     }
 }
+
+
 
