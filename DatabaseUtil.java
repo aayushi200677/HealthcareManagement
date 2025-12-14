baba24e1be8b481da55cd69f1426722d2bdf866a
aayushi200677 <Aayushi.24scse1010298@galgotiasuniversity.ac.in>
2025-12-14 12:53:36 +0530

    Update DatabaseUtil.java

--- DatabaseUtil.java
+++ DatabaseUtil.java
@@ -1,21 +1,32 @@
 package hospital;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
-public class DatabaseUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/hospital";
-    private static final String USER = "root";
-    private static final String PASSWORD = "aayu@2006"; 
+public final class DatabaseUtil {
+    private static final Properties PROPS = new Properties();
+
+    static {
+        try (InputStream in = DatabaseUtil.class.getClassLoader().getResourceAsStream("db.properties")) {
+            if (in == null) throw new IllegalStateException("db.properties not found in classpath");
+            PROPS.load(in);
+            Class.forName(PROPS.getProperty("db.driver", "com.mysql.cj.jdbc.Driver"));
+        } catch (Exception e) {
+            throw new ExceptionInInitializerError("DB init failed: " + e.getMessage());
+        }
+    }
+
+    private DatabaseUtil() { }
 
     public static Connection getConnection() throws SQLException {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); 
-        } catch (ClassNotFoundException ex) {
-            System.out.println("MySQL JDBC Driver not found.");
-            ex.printStackTrace();
-        }
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(
+                PROPS.getProperty("db.url"),
+                PROPS.getProperty("db.user"),
+                PROPS.getProperty("db.password")
+        );
     }
 }
+
